5523a25787eb745708725f8a43a731b778eeda32
Gautam Shaju <74045821+gautamshaju03@users.noreply.github.com>
2024-07-12 02:30:00 +0530

    Delete server directory

--- server/models/token.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-const tokenSchema=mongoose.Schema({
-    token:{
-        type:String,
-        required:true
-    }
-})
-
-module.exports=mongoose.model("token",tokenSchema);
